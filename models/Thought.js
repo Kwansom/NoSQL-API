e93bb574e5eef6fa0aa59234b82af80c653b954d
Kun Wansom <wansom.kun@gmail.com>
2024-12-14 10:13:17 -0800

    setup models

---
+++ models/Thought.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+
+// Reaction schema
+const reactionSchema = new mongoose.Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => timestamp.toISOString(),
+    },
+  },
+  {
+    toJSON: {
+      getters: true, // applies the getter method for timestamp
+    },
+  }
+);
+
+const thoughtSchema = new mongoose.Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => timestamp.toISOString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true, 
+    },
+  }
+);
+
+// Virtual to get reaction count
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model("Thought", thoughtSchema);
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true, // remove any leading or trailing spaces
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+\@.+\..+/, "Please fill a valid email address"], // Regex to validate email format
+    },
+    // Array of thoughts associated with the user, referencing Thought model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId, // each thought is referenced by it's ObjectId
+        ref: "Thought", // acts as foreign key to Thought model
+      },
+    ],
+    // Array of friends (references to other users)
+    friends: [
+      {
+        type: Schema.Types.ObjectId, // referenced by its ObjectId
+        ref: "User", // acts as foreign key to User model (self-reference)
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true, // Enables virtual fields in the output
+    },
+  }
+);
+
+// Virtual to get friend count
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// Using mongooe.model() to compile a model based on the schema 'userSchema'
+const User = mongoose.model("User", userSchema);
+module.exports = User;
